cb3bdaa2087dcc9793759aad0e2944ebc240eff5
Zeeshan <115539434+azeegit@users.noreply.github.com>
2023-10-24 18:45:02 +0100

    first commit

---
+++ src/test/java/com/b1080265/ProjectFEx/ProjectFExApplicationTests.java
@@ -0,0 +1,13 @@
+package com.b1080265.ProjectFEx;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectFExApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
